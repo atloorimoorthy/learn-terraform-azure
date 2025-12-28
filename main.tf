4e5663166cad2ec593d1a9d8becb138a53d11554
atloorimoorthy <atloorimoorthy@gmail.com>
2025-12-28 19:06:53 +0530

    Initial clean Terraform commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "Switch_Job_On_DevOps"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus"
+
+  tags = {
+
+    Environment = "TerraformDemo"
+    Team       = "DevOps"
+  }
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
